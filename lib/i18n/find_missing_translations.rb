b67bd3ae5685ace9850c023ea4025dfdc357265f
Peter Horn <peter.horn@tickettoaster.de>
2017-06-07 13:51:05 +0200

    store missing translations only if file is given

--- lib/i18n/find_missing_translations.rb
+++ lib/i18n/find_missing_translations.rb
@@ -23,14 +23,16 @@ if I18nO7r.save_missing_translations_in_envs.member?(Rails.env.to_s)
       end
       # The key wasn't found and we're prepared to write it away
       return nil if ignored
-      mt_store = YAML::Store.new(I18nO7r.missing_translations_filename)
-      keys = I18n.normalize_keys(locale, key, scope, options[:separator])
-      mt_store.transaction do
-        keys.each.with_index.inject(mt_store) do |a, (e, i)|
-          # puts ">> #{a} -> #{e}, #{i}"
-          a[e.to_s] ||= (i == keys.length-1) ? defaults[locale] : {}
-        end # keys each
-      end # store transaction
+      if I18nO7r.missing_translations_filename
+        mt_store = YAML::Store.new(I18nO7r.missing_translations_filename)
+        keys = I18n.normalize_keys(locale, key, scope, options[:separator])
+        mt_store.transaction do
+          keys.each.with_index.inject(mt_store) do |a, (e, i)|
+            # puts ">> #{a} -> #{e}, #{i}"
+            a[e.to_s] ||= (i == keys.length-1) ? defaults[locale] : {}
+          end # keys each
+        end # store transaction
+      end
       return defaults[locale]
     end # lookup
 
