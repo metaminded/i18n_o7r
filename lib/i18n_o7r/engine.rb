5c47303bed675c3977ead6d866b73dff64514c82
Felix Wolf <11018180+haacs@users.noreply.github.com>
2022-04-05 17:28:30 +0200

    configure assets for precompilation

--- lib/i18n_o7r/engine.rb
+++ lib/i18n_o7r/engine.rb
@@ -1,3 +1,6 @@
 class I18nO7r::Engine < ::Rails::Engine
   isolate_namespace I18nO7r
+  initializer "i18n_o7r.assets.precompile" do |app|
+    app.config.assets.precompile += %w[i18n_o7r_manifest]
+  end
 end
